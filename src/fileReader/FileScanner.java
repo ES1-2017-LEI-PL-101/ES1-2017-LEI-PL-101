01563f0d7d5ff778565bf07f4f66cd62af25b415
bjngs-iscteiul <32638627+bjngs-iscteiul@users.noreply.github.com>
2017-12-06 16:58:18 +0000

    Adicinar caminho no GUI
    Adicionados Listeners dos botoes Browser Rules, Browser Ham, Browser Spam.
    Adicionado caminho dos ficheiros na Interface.
    
    Adaptação no FileScanner

--- src/fileReader/FileScanner.java
+++ src/fileReader/FileScanner.java
@@ -7,18 +7,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class fileScanner {
+public class FileScanner {
 	
 	private static Map<String, String> rulesMap = new HashMap<String, String>();
 	private static Map<String, ArrayList<String>> logMap = new HashMap<String, ArrayList<String>>();
 	
-	public fileScanner(String filePath) {
-		if(filePath.contains("rules.cf")) {
-			readFile(filePath);
-		} else {
-			readLogFile(filePath);
-		}
-	}
+//	public fileScanner(String filePath) {
+//		if(filePath.contains("rules.cf")) {
+//			readFile(filePath);
+//		} else {
+//			readLogFile(filePath);
+//		}
+//	}
 	
 	//Write on hashmap the content of the file
 	public static void readFile(String filePath) {
@@ -41,6 +41,7 @@ public class fileScanner {
 				e.printStackTrace();
 			}
 		}
+		System.out.println(getRulesMap());
 	} 
 	
 	public static void readLogFile(String filePath) { 
@@ -71,14 +72,14 @@ public class fileScanner {
 	}
 	
 	//Test file to hashMap
-	public static void main(String[] args) {
-		fileScanner rulesFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
-		fileScanner hamFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
-		fileScanner spamFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/spam.log");
-		System.out.println(rulesFileScanner.getRulesMap());
-		System.out.println(hamFileScanner.getLogMap());
-		System.out.println(spamFileScanner.getLogMap());
-	}
+//	public static void main(String[] args) {
+//		fileScanner rulesFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
+//		fileScanner hamFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
+//		fileScanner spamFileScanner = new fileScanner("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/spam.log");
+//		System.out.println(rulesFileScanner.getRulesMap());
+//		System.out.println(hamFileScanner.getLogMap());
+//		System.out.println(spamFileScanner.getLogMap());
+//	}
 	
 	public static Map<String, String> getRulesMap() {
 		return rulesMap;

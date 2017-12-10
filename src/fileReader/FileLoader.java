6d70db6769abd46f845f603aab0f321a8f16c0fd
Joana Pascoal <jicpl@iscte-iul.pt>
2017-12-10 01:35:52 +0000

    Refactor and Manual Configuration
    - Changed HashMap  to LinkedHashMap
    - Verification of existing weights on rules ? start to 0.0
    - deleted rulesLazer and rulesMista
    - changed rules, ham and spam to Files folder
    - writeFile function implemented
    - Added listeners on test and save test
    - Changed Browser Listener
    -Show rules on Table -> GUI
    - Update Table implemented
    - Edit weight rules by GUI implemented
    - GUI adjustments

--- src/fileReader/FileLoader.java
+++ src/fileReader/FileLoader.java
@@ -1,34 +1,46 @@
 package fileReader;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-
+import java.util.Map.Entry;
 
 public class FileLoader {
-	
-	private static HashMap <String,Double> rulesMap = new HashMap<String, Double>();
-	private static HashMap <String, ArrayList<String>> HamRulesMap = new HashMap<String, ArrayList<String>>();
-	private static HashMap <String, ArrayList<String>> SpamRulesMap = new HashMap<String, ArrayList<String>>();
+
+	private static LinkedHashMap<String, Double> rulesMap = new LinkedHashMap<String, Double>();
+	private static LinkedHashMap<String, ArrayList<String>> HamRulesMap = new LinkedHashMap<String, ArrayList<String>>();
+	private static LinkedHashMap<String, ArrayList<String>> SpamRulesMap = new LinkedHashMap<String, ArrayList<String>>();
 	private static final FileLoader INSTANCE = new FileLoader();
 
-		
 	public static FileLoader getInstance() {
 		return INSTANCE;
 	}
 
-	//Write on hashmap the content of the file
+	// Write on hashmap the content of the file
 	public static void readFile(String filePath) {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filePath));
 			String sCurrentLine;
 			while ((sCurrentLine = br.readLine()) != null) {
-				rulesMap.put(sCurrentLine, -1.0);
-				//System.out.println(sCurrentLine);
+				String[] line = sCurrentLine.split(" ");
+				if (line.length > 1) {
+					rulesMap.put(line[0], Double.parseDouble(line[1]));
+				} else {
+					rulesMap.put(sCurrentLine, 0.0);
+				}
+
+				// System.out.println(sCurrentLine);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -36,14 +48,14 @@ public class FileLoader {
 			try {
 				if (br != null) {
 					br.close();
-				}			
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-	} 
-	
-	public static void readLogFile(String filePath) { 
+	}
+
+	public static void readLogFile(String filePath) {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filePath));
@@ -51,16 +63,16 @@ public class FileLoader {
 			while ((sCurrentLine = br.readLine()) != null) {
 				String[] parts = sCurrentLine.split("	");
 				ArrayList<String> a = new ArrayList<String>();
-				for(int i=1; i<parts.length; i++) {
+				for (int i = 1; i < parts.length; i++) {
 					a.add(parts[i]);
 				}
-				if(filePath.contains("spam.log")) {
+				if (filePath.contains("spam.log")) {
 					SpamRulesMap.put(parts[0], a);
 				} else {
 					HamRulesMap.put(parts[0], a);
 				}
-				
-				//System.out.println(sCurrentLine);
+
+				// System.out.println(sCurrentLine);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -68,44 +80,67 @@ public class FileLoader {
 			try {
 				if (br != null) {
 					br.close();
-				}			
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	//Test file to hashMap
-//	public static void main(String[] args) {
-//		FileLoader.getInstance().readFile("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
-//		FileLoader.getInstance().readLogFile("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
-//		System.out.println(FileLoader.getInstance().getRulesMap().toString());
-//		System.out.println(FileLoader.getInstance().getHamRulesMap().toString());
-//		
-//		//FileLoader rulesFileScanner = new FileLoader("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
-//		//FileLoader hamFileScanner = new FileLoader("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
-//		//FileLoader spamFileScanner = new FileLoader("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/spam.log");
-//		//System.out.println(rulesFileScanner.getRulesMap());
-//		//System.out.println(hamFileScanner.getLogMap());
-//		//System.out.println(spamFileScanner.getLogMap());
-//	}
-	
-	public HashMap<String, Double> getRulesMap() {
+
+	// Test file to hashMap
+	// public static void main(String[] args) {
+	// FileLoader.getInstance().readFile("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
+	// FileLoader.getInstance().readLogFile("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
+	// System.out.println(FileLoader.getInstance().getRulesMap().toString());
+	// System.out.println(FileLoader.getInstance().getHamRulesMap().toString());
+	//
+	// //FileLoader rulesFileScanner = new
+	// FileLoader("AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/rules.cf");
+	// //FileLoader hamFileScanner = new
+	// FileLoader("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/ham.log");
+	// //FileLoader spamFileScanner = new
+	// FileLoader("/ES1-2017/AntiSpamConfigurationForBalancedProfessionalAndLeisureMailbox/spam.log");
+	// //System.out.println(rulesFileScanner.getRulesMap());
+	// //System.out.println(hamFileScanner.getLogMap());
+	// //System.out.println(spamFileScanner.getLogMap());
+	// }
+
+	public LinkedHashMap<String, Double> getRulesMap() {
 		return rulesMap;
 	}
 
-	public HashMap<String, ArrayList<String>> getHamRulesMap() {
+	public LinkedHashMap<String, ArrayList<String>> getHamRulesMap() {
 		return HamRulesMap;
 	}
 
-	public HashMap<String, ArrayList<String>> getSpamRulesMap() {
+	public LinkedHashMap<String, ArrayList<String>> getSpamRulesMap() {
 		return SpamRulesMap;
 	}
-	
 
+	public static void writeFile(String filePath, LinkedHashMap <String,Double> rules) {
+
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath), "utf-8"))) {
+			for (Entry<String, Double> entry : rules.entrySet()) {
+				String key = entry.getKey();
+				Double value = entry.getValue();
+				writer.write(key.toString() + " " + value.toString());
+				writer.write(System.lineSeparator());
+			}
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public void setRules(LinkedHashMap<String, Double> newRules) {
+		this.rulesMap = newRules;
+	}
 
 }
-
-		
-	

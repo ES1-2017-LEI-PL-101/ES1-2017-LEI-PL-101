f6548d10f98f961d74cc1db0b7a379e970ab6d3b
bjngs-iscteiul <32638627+bjngs-iscteiul@users.noreply.github.com>
2017-12-15 20:07:23 +0000

    Revert "Revert "javadocs and refactor""
    This reverts commit 1a13d3e93bfedb60c7c9408711cd4c89f81c95cc, reversing
    changes made to 7ea9f7f88a9b83883e13afcca4eaf9d9e3aed895.

--- src/antiSpamFilter/AntiSpamFilterAutomaticConfiguration.java
+++ src/antiSpamFilter/AntiSpamFilterAutomaticConfiguration.java
@@ -24,70 +24,59 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AntiSpamFilterAutomaticConfiguration {
-  private static final int INDEPENDENT_RUNS = 5 ;
+	private static final int INDEPENDENT_RUNS = 5;
 
-  public static void main(String[] args) throws IOException {
-	Debug.IN("AntiSpamFilterAutomaticConfiguration [MAIN]");
-    String experimentBaseDirectory = "experimentBaseDirectory";
+	public static void main(String[] args) throws IOException {
+		Debug.in("AntiSpamFilterAutomaticConfiguration [MAIN]");
+		String experimentBaseDirectory = "experimentBaseDirectory";
 
-    List<ExperimentProblem<DoubleSolution>> problemList = new ArrayList<>();
-    
-    problemList.add(new ExperimentProblem<>(new AntiSpamFilterProblem(), "AntiSpamFilterProblem"));
-    
-    //Debug.getInstance().msg("ProblemList"+problemList.toString());
-    
-    List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithmList =
-            configureAlgorithmList(problemList);
-    
-    //Debug.getInstance().msg("algorithmList"+algorithmList.toString());
-    
-    Experiment<DoubleSolution, List<DoubleSolution>> experiment =
-        new ExperimentBuilder<DoubleSolution, List<DoubleSolution>>("AntiSpamStudy")
-            .setAlgorithmList(algorithmList)
-            .setProblemList(problemList)
-            .setExperimentBaseDirectory(experimentBaseDirectory)
-            .setOutputParetoFrontFileName("FUN")
-            .setOutputParetoSetFileName("VAR")
-            .setReferenceFrontDirectory(experimentBaseDirectory+"/referenceFronts")
-            .setIndicatorList(Arrays.asList(
-            		new PISAHypervolume<DoubleSolution>()
-            	))
-            	.setIndependentRuns(INDEPENDENT_RUNS)
-            	.setNumberOfCores(8)
-            	.build();
-    
-    //Debug.getInstance().msg("experiment"+experiment.toString());
-    
-    new ExecuteAlgorithms<>(experiment).run();
-    new GenerateReferenceParetoSetAndFrontFromDoubleSolutions(experiment).run();
-    new ComputeQualityIndicators<>(experiment).run() ;
-    new GenerateLatexTablesWithStatistics(experiment).run() ;
-    new GenerateBoxplotsWithR<>(experiment).setRows(1).setColumns(1).run() ;
-    
-    Debug.OUT("AntiSpamFilterAutomaticConfiguration [MAIN]");
-  }
+		List<ExperimentProblem<DoubleSolution>> problemList = new ArrayList<>();
 
-  static List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> configureAlgorithmList(
-          List<ExperimentProblem<DoubleSolution>> problemList) {
-	  Debug.IN("AntiSpamFilterAutomaticConfiguration [configureAlgorithmList]");
-   
-	  List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithms = new ArrayList<>();
+		problemList.add(new ExperimentProblem<>(new AntiSpamFilterProblem(), "AntiSpamFilterProblem"));
 
-	  //Debug.getInstance().msg("algorithms "+algorithms.toString());
-    for (int i = 0; i < problemList.size(); i++) {
-      Algorithm<List<DoubleSolution>> algorithm = 
-    		  new NSGAIIBuilder<>(
-					              problemList.get(i).getProblem(),
-					              new SBXCrossover(1.0, 5),
-					              new PolynomialMutation(1.0 / problemList.get(i).getProblem().getNumberOfVariables(), 10.0)
-				  				).setMaxEvaluations(25000)
-    		  					.setPopulationSize(100)
-    		  					.build();
-      algorithms.add(new ExperimentAlgorithm<>(algorithm, "NSGAII", problemList.get(i).getTag()));
-      //Debug.getInstance().msg("algorithms "+algorithms.toString());
-    }
-    Debug.OUT("AntiSpamFilterAutomaticConfiguration [configureAlgorithmList]");
-    return algorithms;
-  }
+		// Debug.getInstance().msg("ProblemList"+problemList.toString());
+
+		List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithmList = configureAlgorithmList(
+				problemList);
+
+		// Debug.getInstance().msg("algorithmList"+algorithmList.toString());
+
+		Experiment<DoubleSolution, List<DoubleSolution>> experiment = new ExperimentBuilder<DoubleSolution, List<DoubleSolution>>(
+				"AntiSpamStudy").setAlgorithmList(algorithmList).setProblemList(problemList)
+				.setExperimentBaseDirectory(experimentBaseDirectory).setOutputParetoFrontFileName("FUN")
+				.setOutputParetoSetFileName("VAR")
+				.setReferenceFrontDirectory(experimentBaseDirectory + "/referenceFronts")
+				.setIndicatorList(Arrays.asList(new PISAHypervolume<DoubleSolution>()))
+				.setIndependentRuns(INDEPENDENT_RUNS).setNumberOfCores(8).build();
+
+		// Debug.getInstance().msg("experiment"+experiment.toString());
+
+		new ExecuteAlgorithms<>(experiment).run();
+		new GenerateReferenceParetoSetAndFrontFromDoubleSolutions(experiment).run();
+		new ComputeQualityIndicators<>(experiment).run();
+		new GenerateLatexTablesWithStatistics(experiment).run();
+		new GenerateBoxplotsWithR<>(experiment).setRows(1).setColumns(1).run();
+
+		Debug.out("AntiSpamFilterAutomaticConfiguration [MAIN]");
+	}
+
+	static List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> configureAlgorithmList(
+			List<ExperimentProblem<DoubleSolution>> problemList) {
+		Debug.in("AntiSpamFilterAutomaticConfiguration [configureAlgorithmList]");
+
+		List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithms = new ArrayList<>();
+
+		// Debug.getInstance().msg("algorithms "+algorithms.toString());
+		for (int i = 0; i < problemList.size(); i++) {
+			Algorithm<List<DoubleSolution>> algorithm = new NSGAIIBuilder<>(problemList.get(i).getProblem(),
+					new SBXCrossover(1.0, 5),
+					new PolynomialMutation(1.0 / problemList.get(i).getProblem().getNumberOfVariables(), 10.0))
+					.setMaxEvaluations(25000).setPopulationSize(100).build();
+			algorithms.add(new ExperimentAlgorithm<>(algorithm, "NSGAII", problemList.get(i).getTag()));
+			// Debug.getInstance().msg("algorithms "+algorithms.toString());
+		}
+		Debug.out("AntiSpamFilterAutomaticConfiguration [configureAlgorithmList]");
+		return algorithms;
+	}
 
 }
